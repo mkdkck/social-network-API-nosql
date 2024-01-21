08ba023682053e5f4407675431af8f514c774543
mkdkck <colin173@gmail.com>
2024-01-22 00:14:44 +1030

    basic controller structure done

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -1,2 +1,61 @@
 const { ObjectId } = require("mongoose").Types;
 const { Thought, User } = require("../models");
+
+
+module.exports = {
+    async getThoughts(req, res) {
+        try {
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async getSingleThought(req, res) {
+        try {
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async createThought(req, res) {
+        try {
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async updateThought(req, res) {
+        try {
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async deleteThought(req, res) {
+        try {
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async addReactionToThought(req, res) {
+        try {
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async deleteReactionFromThought(req, res) {
+        try {
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+}
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,3 +1,60 @@
 const { ObjectId } = require("mongoose").Types;
 const { Thought, User } = require("../models");
 
+module.exports = {
+    async getUsers(req, res) {
+        try {
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async getSingleUser(req, res) {
+        try {
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async createUser(req, res) {
+        try {
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async updateUser(req, res) {
+        try {
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async deleteUser(req, res) {
+        try {
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async addFriendToUser(req, res) {
+        try {
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async deleteFriendFromUser(req, res) {
+        try {
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+}
